c7c80a864f457ef2a9b99d790e42377e00f39442
UNIVERSE\msikora <maciej.sikora@dataart.com>
2018-03-24 16:06:12 +0100

    a and b

--- src/a.js
+++ src/a.js
@@ -2,9 +2,20 @@ console.log('I am a.js!');
 
 document.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded. Ready to go!");
+  a1();
 });
 
 // A.1 - Stworz licznik ktory nalicza numery co 1 sekunde i wstawia liczbe to html
+const a1 = () => {
+  const span = document.querySelector('span#counter');
+  let counter = 0;
+  span.innerText = counter;
+  
+  setInterval(() => {
+    counter++;
+    span.innerText = counter;
+  }, 1000);
+};
 // A.2 - Stworz minutnik ktory posiada strukture 00:00 - inkrementuje sekundy, pokazuje ile uplynelo minut i sekund
 // A.3 - Stworz funkcje ktora na wejsciu dostaje co ile minutnik ma sie inkrementowac - po wywolaniu
 // ma dodac do strony kolejny minutnik ktory inkrementuje zgodnie z zadanym argumentem
